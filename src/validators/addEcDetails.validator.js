b1a7b9dec79e975717415128e22bc2d09205fc47
prajwalkeerthi-rhibhus <prajwal.keerthi@rhibhus.com>
2024-04-04 15:49:03 +0530

    added

--- src/validators/addEcDetails.validator.js
+++ src/validators/addEcDetails.validator.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 const { keyWords } = require('../constants/constant');
 
 const addEcDetailsSchema = Joi.object({
-    EC_name: Joi.string().required().label(keyWords.ECDETAIL_NAME),
+    ecName: Joi.string().required().label(keyWords.ECDETAIL_NAME),
     remarks : Joi.string()
 });
 
