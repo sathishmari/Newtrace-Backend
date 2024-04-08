5e141d7ab545b5cb7140b59f3824e037b2fab5e6
unknown <sathish.mariappa@rhibhus.com>
2024-04-08 14:39:35 +0530

    08-apr

--- cosmosConfig.js
+++ cosmosConfig.js
@@ -17,10 +17,13 @@ dotenv.config();
 
 // ===================================== LOCAL =================================================
 
-console.log("COSMOS_ACC_NAME : ", process.env.COSMOS_ACC_NAME);
+// console.log("COSMOS_ACC_NAME : ", process.env.COSMOS_ACC_NAME);
 
-const endpoint = `https://127.0.0.1:8081/`;
-const key = `C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==`;
+// const endpoint = `https://127.0.0.1:8081/`;
+// const key = `C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==`;
+
+const endpoint = `https://dev-newtrace-db.documents.azure.com:443/`;
+const key = `YeKnDr7zsQZD0YiuYi20g1mxyzw5wnVfJiCjjwU0Oe2J1wVMlcQQMAkb7TM0QazBAXySOSuv0zfCACDbidnVhw==`;
 
 // const client = new CosmosClient({ endpoint, key });
 
