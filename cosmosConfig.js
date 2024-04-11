c857bc0c5a7fef68216c2f6f84c8cffc6e74d6c0
unknown <sathish.mariappa@rhibhus.com>
2024-04-11 14:30:00 +0530

    11-apr

--- cosmosConfig.js
+++ cosmosConfig.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 // console.log("COSMOS_ACC_NAME : ", process.env.COSMOS_ACC_NAME);
 
-// const endpoint = `https://${process.env.COSMOS_ACC_NAME}.documents.azure.com:443/`;
+// const endpoint = `https://${process.env.COSMOS_DB_NAME}.documents.azure.com:443/`;
 // const key = process.env.COSMOS_KEY;
 
 // const client = new CosmosClient({ endpoint, key });
@@ -17,13 +17,16 @@ dotenv.config();
 
 // ===================================== LOCAL =================================================
 
-// console.log("COSMOS_ACC_NAME : ", process.env.COSMOS_ACC_NAME);
+console.log("COSMOS_ACC_NAME : ", process.env.COSMOS_ACC_NAME);
 
 // const endpoint = `https://127.0.0.1:8081/`;
 // const key = `C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==`;
 
-const endpoint = `https://dev-newtrace-db.documents.azure.com:443/`;
-const key = `YeKnDr7zsQZD0YiuYi20g1mxyzw5wnVfJiCjjwU0Oe2J1wVMlcQQMAkb7TM0QazBAXySOSuv0zfCACDbidnVhw==`;
+// const endpoint = `https://dev-newtrace-db.documents.azure.com:443/`;
+// const key = `YeKnDr7zsQZD0YiuYi20g1mxyzw5wnVfJiCjjwU0Oe2J1wVMlcQQMAkb7TM0QazBAXySOSuv0zfCACDbidnVhw==`;
+
+const endpoint = `https://${process.env.COSMOS_DB_NAME}.documents.azure.com:443/`;
+const key = process.env.COSMOS_KEY;
 
 // const client = new CosmosClient({ endpoint, key });
 
